adc8b3432a039ad35e8166248a98c1ac83969dab
jacquelynjg <jacquelyn.jacinto@gmail.com>
2020-10-13 13:16:42 +0800

    WIP: Google Play correction

--- js/google.js
+++ js/google.js
@@ -1,5 +1,6 @@
 if (/(android)/i.test(navigator.userAgent)) {
   document.querySelectorAll('a[href]').forEach(function (link) {
+    link.href = link.href.replace('http://play.google.com/store/apps/','market://');
     link.href = link.href.replace('https://play.google.com/store/apps/','market://');
   });
 }
